3cbbbdc11aa028090f48e72460eeecfe94ac0bb8
diegosmp <diiegosmp@gmail.com>
2024-07-28 23:45:46 -0300

    Feature: updated post

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { Post } from '../models/Post'
 import { User } from '../models/User'
+import { where } from 'sequelize'
 
 export default class PostController {
    static async createPost(req: Request, res: Response) {
@@ -28,6 +29,56 @@ export default class PostController {
          })
 
          return res.status(201).json({ message: 'Post created successfully!', newPost })
+      } catch (error) {
+         console.error(error)
+         return res.status(500).json({ message: 'Error connect server!' })
+      }
+   }
+
+   static async showAllPost(req: Request, res: Response) {
+      const posts = await Post.findAll()
+      return res.status(200).json({ posts })
+   }
+
+   static async editPost(req: Request, res: Response) {
+      const { title, imgPost, post, description } = req.body
+      const { postId } = req.params
+      const postExist: any = await Post.findByPk(postId)
+
+      const userAuth = postExist.userId
+
+      if (userAuth !== req.user.id) {
+         return res.status(422).json({ message: 'User unauthorized!' })
+      }
+
+      if (!postExist) {
+         return res.status(404).json({ message: 'Post not exist!' })
+      }
+
+      if (!title) {
+         return res.status(422).json({ message: 'The field title is required!' })
+      }
+
+      if (!post) {
+         return res.status(422).json({ message: 'The field post is required!' })
+      }
+
+      if (!description) {
+         return res.status(422).json({ message: 'The field description is required!' })
+      }
+
+      try {
+         await Post.update(
+            {
+               title,
+               imgPost,
+               post,
+               description,
+            },
+            { where: { id: postId } },
+         )
+
+         return res.status(201).json({ message: 'Updated post successfully!' })
       } catch (error) {
          console.error(error)
          return res.status(500).json({ message: 'Error connect server!' })
--- src/routes/PostRoute.ts
+++ src/routes/PostRoute.ts
@@ -5,5 +5,7 @@ import PostController from '../controllers/PostController'
 const router = express.Router()
 
 router.post('/create', authUser, PostController.createPost)
+router.get('/all', authUser, PostController.showAllPost)
+router.patch('/edit/:postId', authUser, PostController.editPost)
 
 export default router
