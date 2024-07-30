557aca871b59a2d61549d24f7973d9d8dfbb04ca
diegosmp <diiegosmp@gmail.com>
2024-07-30 10:38:38 -0300

    Additional cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import { sequelize as conn } from '../config/sequelize'
 import UserRoutes from './routes/UserRoute'
 import PostRoutes from './routes/PostRoute'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
-
+app.use(cors({ origin: 'http://localhost:5050', credentials: true }))
 app.use(express.json())
 
 app.use('/users', UserRoutes)
